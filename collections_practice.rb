c29d65bcd1df3df63f7db550a9a7d01eff073bf3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 18:23:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,7 +53,7 @@ def add_s(array)
   counter =
   array.each_with_index do |word, index|
     word.split
-    word[length-1] = 's' unless word
+    word[length-1] = 's' unless index == 1
 end
 return new_array
 end
