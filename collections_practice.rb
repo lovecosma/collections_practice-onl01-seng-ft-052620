df41f3c9191ad11c647310f7ac8c707b3bddfe74
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 17:48:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length<=>b.length
+  end
+end
+
+def swap_elements_from_to(array, index, index_dest)
+array[index], array[index_dest] = array[index_dest], array[index]
+array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |word|
+    
+end
