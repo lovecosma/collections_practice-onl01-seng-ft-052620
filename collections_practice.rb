a7c8eb4a8976b04c62631afd3e773d710dfee7d1
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 18:12:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def find_a(array)
   new_array = []
   array.each do |word|
     word.split
-     new_array << word if word[0] == "a"  
+     new_array << word if word[0] == "a"
 end
 new_array
 end
